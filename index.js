d29b30d7338e91c98915fb593f96844c6dd0e40b
Richard Mora <llody777@gmail.com>
2025-11-13 15:09:51 -0800

    Write comments

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 
+// write message into message.txt
 // fs.writeFile("message.txt", "Hello again from Node.js", (err) => {
 //     if (err) throw err;
 //     console.log("The file has been saved!");
 // });
 
+// read what's on message.txt instead of writing in message.txt
 fs.readFile("message.txt", "utf-8", (err, data) =>{
     if (err) throw err;
     console.log(data);
